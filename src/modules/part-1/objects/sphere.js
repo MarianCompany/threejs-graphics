2ba89e175cfe2f0953e2ff499ee8dea58507161c
MarianCompany <samojlenko.wot@gmail.com>
2026-01-08 22:42:42 +0300

    feat: added image to mesh

--- src/modules/part-1/objects/sphere.js
+++ src/modules/part-1/objects/sphere.js
@@ -1,7 +1,16 @@
 import * as THREE from "three";
+import image from '../../../assets/mesh.jpg'
 
 export const createSphere = () => {
     const geometry = new THREE.SphereGeometry(1, 32, 32);
-    const material = new THREE.MeshStandardMaterial({ color: 0x00ffcc });
+
+    const texture = new THREE.TextureLoader().load(image);
+
+    const material = new THREE.MeshStandardMaterial({
+        map: texture,
+        roughness: 1,
+        metalness: 0.2
+    });
+
     return new THREE.Mesh(geometry, material);
-}
+};
